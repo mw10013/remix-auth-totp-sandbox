c7f7659da202ec980543f45b8e16c3ece3ef921c
Michael Wu <mw10013@gmail.com>
2023-12-13 00:39:11 -0500

    authenticator

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,48 @@
-import type { MetaFunction } from "@remix-run/cloudflare";
+import {
+  createCookieSessionStorage,
+  type MetaFunction,
+} from "@remix-run/cloudflare";
+import { useLoaderData } from "@remix-run/react";
+import { Authenticator } from "remix-auth";
+import { TOTPStrategy } from "remix-auth-totp";
+
+interface User {
+  id: string;
+  email: string;
+}
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: "Remix Auth Totp Sandbox" },
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
 
+export function loader() {
+  const authSessionStorage = createCookieSessionStorage({
+    cookie: {
+      name: "_auth",
+      path: "/",
+      sameSite: "lax",
+      httpOnly: true,
+      secrets: ["SECRET"],
+      secure: false,
+    },
+  });
+  const authenticator = new Authenticator<User>(authSessionStorage, {
+    throwOnError: true,
+  });
+  return {
+    authenticator
+  }
+}
+
 export default function Index() {
+  const data = useLoaderData<typeof loader>();
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <h1>Welcome to Remix Auth Totp Sandbox</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
 }
